71e95eef5018b69da5d3f7f80be67e0a406adc04
Gugizm <93256566+Gugizm@users.noreply.github.com>
2024-04-25 16:51:32 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 
+# Telegram
+
+# Telegram Bot API token
+TELEGRAM_BOT_TOKEN = False
+
+# Telegram Group ID
+TELEGRAM_GROUP_CHAT_ID = False
+
+
+# URL of web page tp scrape
+WEB_PAGE_URL = """https://sslecal2.investing.com?columns=exc_currency,exc_actual,
+exc_forecast,exc_previous&importance=3&countries=5&calType=day&timeZone=73&lang=1"""
